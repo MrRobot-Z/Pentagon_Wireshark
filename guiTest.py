010ca17964d54846eca8a127e3cc1c83603c25ed
MahmoudSalah17 <midozsalah@gmail.com>
2017-12-19 21:07:41 +0200

    GUI update 2

--- guiTest.py
+++ guiTest.py
@@ -1,3 +1,10 @@
+def ay7aga():
+    s = ui.ListView.selectedItems()
+    if s:
+        packet_no = s[0].text(0)
+        Detailchild.setText(0, "Detail for Packet No. " + str(packet_no))
+        ui.HexView.setText("Hex Data for packet No. " + str(packet_no))
+
 if __name__ == "__main__":
     import sys
     from gui import *
@@ -7,19 +14,16 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     child = QtWidgets.QTreeWidgetItem(ui.ListView)
     child2 = QtWidgets.QTreeWidgetItem(ui.ListView)
-    child.setText(0, "Da ID")
+    child.setText(0, "0")
     child.setText(1, "da Time")
     child.setText(2, "da Source")
     child.setText(3, "da Dest")
-    child2.setText(0, "da tany time")
+    child2.setText(0, "1")
     Detailchild = QtWidgets.QTreeWidgetItem(ui.DetailView)
     Detailchild.setText(0, "skjdgjdfghdfjg")
     subchild = QtWidgets.QTreeWidgetItem(Detailchild)
     subchild.setText(0, "sdjjhsihgsakjf")
     ui.DetailView.expandAll()
-    ui.DetailView.itemClicked(child2).connect(ay7aga)
+    ui.ListView.itemClicked.connect(ay7aga)
     MainWindow.show()
     sys.exit(app.exec_())
-
-def ay7aga():
-    ui.HexView.setText("A7aaaaaaaaaaaaaaaaaaa")
