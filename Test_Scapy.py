f670847feea65e74153c4610158cec0b84912ebb
Moustafa Zaher <m.zaher2030@gmail.com>
2017-12-24 13:27:39 +0200

    Use the Test_Scapy.py file to test if the Library Scapy3k is funcyioning properly

--- Test_Scapy.py
+++ Test_Scapy.py
@@ -1,34 +1,20 @@
 import scapy.all as spy
 import Wireshark_utils as WsU
-from threading import Thread
-import time
 
 print("Scapy is Imported")
-# packets = spy.rdpcap("example_network_traffic.pcap")
-
-packets = []
 
 
-def test_sniff():
-    global packets
-    packets.append(spy.sniff(filter="tcp port 80", timeout=20))
-    print("Packet Added =D ", len(packets))
-
-
-t = Thread(target=test_sniff)
-print("Thread Started")
-t.start()
-
-time.sleep(10)
-print("I'm back")
-try:
-    t._stop()
-except AssertionError:
-    pass
-
-print(len(packets))
-for one in packets:
-    print("I'm In heeeeeeeeeere")
+rd_packets = spy.rdpcap("example_network_traffic.pcap")
+print(len(rd_packets))
+for one in rd_packets:
     p = WsU.get_show_data(one)
     for i in p:
         print(i)
+spy.hexdump(rd_packets[0])
+
+
+print("Start Sniffing")
+packets = spy.sniff(filter="", timeout=10)
+for i in packets:
+    print(i.summary())
+print(len(packets))
